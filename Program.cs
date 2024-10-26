9e670ffc47a288a97b85191a7e12cfb1275b71d6
TaliaGoncalves <gtalia246@gmail.com>
2024-10-26 17:30:33 -0300

    feat: criando program para executar game

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Escolha o modo de jogo: (1) PvE (2) PvP");
+        int mode = int.Parse(Console.ReadLine() ?? "1");
+
+        bool isPvE = mode == 1;
+
+        Game game = new Game(isPvE);
+        
+        // Adicionando monstros ao jogo
+        game.AddMonster(new Dragon());
+        game.AddMonster(new Robot());
+        game.AddMonster(new Zombie());
+
+        // Iniciando o jogo
+        game.Start();
+    }
+}
