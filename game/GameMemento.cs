c622276aed59242fdfec246700ba008df1faf178
Leticiamatos22 <cristinadematosleticia2@gmail.com>
2024-11-18 21:24:23 -0300

    refactor: alterando codigo para conseguir implementar o desing do memento

--- game/GameMemento.cs
+++ game/GameMemento.cs
@@ -1,15 +1,21 @@
+GameMemento.cs:
 using BatalhaDeMonstros.Monsters;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace BatalhaDeMonstros.Memento
 {
+    [Serializable]
     public class GameMemento
     {
         public List<Monster> Monsters { get; }
+        public bool IsPvE { get; }  // Adicionando a propriedade IsPvE para indicar o modo de jogo
 
-        public GameMemento(List<Monster> monsters)
+        public GameMemento(List<Monster> monsters, bool isPvE)
         {
             Monsters = monsters.Select(m => new Monster(m.Health, m.AttackPower, m.Defense)).ToList();
+            IsPvE = isPvE;  // Armazena o modo de jogo
         }
     }
 }
