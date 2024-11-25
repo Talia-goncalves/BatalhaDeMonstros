53be2637c3053524b85caf23e0141497acbf818b
Talia-goncalves <gtalia246@gmail.com>
2024-11-25 19:58:03 -0300

    docs: adicionando comentarios dedocumentacao ao actions

--- actions/DefendAction.cs
+++ actions/DefendAction.cs
@@ -2,11 +2,16 @@ using BatalhaDeMonstros.Monsters;
 
 namespace BatalhaDeMonstros.Actions
 {
+    // Classe que define a ação de defesa de um monstro.
     public class DefendAction : ICombatAction
     {
+        // Executa a defesa, aumentando a defesa do monstro atacante.
         public void Execute(Monster attacker, Monster defender)
         {
+            // Exibe uma mensagem indicando que o monstro está se defendendo.
             Console.WriteLine($"{attacker.GetType().Name} está se defendendo!");
+
+            // Aumenta a defesa do monstro atacante.
             attacker.IncreaseDefense(5);
         }
     }
